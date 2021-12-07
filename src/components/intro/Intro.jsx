3a45a3cdccf9e45257b76cc04ac7eab8241d669f
WesamHaddad125 <wesam.haddad14@gmail.com>
2021-12-07 02:25:22 -0500

    finished intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,35 @@
-import "./intro.scss"
-export default function intro() {
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
+
+export default function Intro() {
+    const textRef = useRef();
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            strings: ["Graduating 2022", "Software Designer", "Software Developer"],
+            backDelay: 1500,
+            backSpeed: 60,
+        });
+    },[]);
+
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Wesam Haddad</h1>
+                    <h3>College Student <span ref={textRef}></span></h3> 
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
