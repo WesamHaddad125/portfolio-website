90500788344d3ab0444edd4ff28e1d6985ee86bc
WesamHaddad125 <wesam.haddad14@gmail.com>
2021-12-08 23:37:32 -0500

    finished portfolio

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,22 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div className="right">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,188 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Rampa UI Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 3,
+      title: "E-commerce Web Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 4,
+      title: "Relax Mobile App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 5,
+      title: "Hero Web Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 6,
+      title: "Banking App",
+      img:
+        "assets/websiteMock.png",
+    },
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "Web Social Media App",
+      img: "assets/websiteMock.png",
+    },
+    {
+      id: 2,
+      title: "Web Rampa UI Design",
+      img: "assets/websiteMock.png",
+    },
+    {
+      id: 3,
+      title: "Web E-commerce Design",
+      img: "assets/websiteMock.png",
+    },
+    {
+      id: 4,
+      title: "Web Relax App",
+      img: "assets/websiteMock.png",
+    },
+    {
+      id: 5,
+      title: "Web Design",
+      img: "assets/websiteMock.png",
+    },
+    {
+      id: 6,
+      title: "Web Banking App",
+      img: "assets/websiteMock.png",
+    },
+  ];
+  
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "Mobile Social Media App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 2,
+      title: "Mobile Rampa UI Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 3,
+      title: "Mobile E-commerce Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 4,
+      title: "Mobile Relax App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 5,
+      title: "Mobile Hero Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 6,
+      title: "Mobile Banking App",
+      img:
+        "assets/websiteMock.png",
+    },
+  ];
+  
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "Design Social Media App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 2,
+      title: "Design Rampa UI Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 3,
+      title: "Design E-commerce Web Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 4,
+      title: "Design Relax Mobile App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 5,
+      title: "Design Keser Web Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 6,
+      title: "Design Banking App",
+      img:
+        "assets/websiteMock.png",
+    },
+  ];
+  
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "Content Social Media App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 2,
+      title: "Content Rampa UI Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 3,
+      title: "Content E-commerce Web Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 4,
+      title: "Content Relax Mobile App",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 5,
+      title: "Content Keser Web Design",
+      img:
+        "assets/websiteMock.png",
+    },
+    {
+      id: 6,
+      title: "Content Banking App",
+      img:
+        "assets/websiteMock.png",
+    },
+  ];
